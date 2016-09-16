b8abb76ed0a309681b1e5fac95de6829222bdc69
Abbas Raza <abbas.raza.1707@gmail.com>
2016-09-16 11:59:36 +1000

    add usage
    Signed-off-by: Abbas Raza <abbas.raza.1707@gmail.com>

--- factors.c
+++ factors.c
@@ -1,3 +1,14 @@
+/* 	Author: Abbas Raza (abbas.raza.1707@gmail.com)
+ *
+ * Usage: ./factors 15 91 88 192
+ *
+ * Factors of 15 are:	1,3,5,15
+ * Factors of 91 are:	1,7,13,91
+ * Factors of 88 are:	1,2,4,8,11,22,44,88
+ * Factors of 192 are:	1,2,3,4,6,8,12,16,24,32,48,64,96,192
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
